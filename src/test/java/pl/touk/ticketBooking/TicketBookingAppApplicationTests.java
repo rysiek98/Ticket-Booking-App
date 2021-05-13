8d7fd8fa847df4c1b88d27dec86c68cd71eb15ee
rysiek98 <m.ryszka98@gmail.com>
2021-05-13 22:58:51 +0200

    Build template with spring initializer

---
+++ src/test/java/pl/touk/ticketBooking/TicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.touk.ticketBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
