40708abd92030149dc429812971cace61ee10893
michal <m.ryszka98@gmail.com>
2021-07-23 08:51:54 +0200

    Changed class ValidateInput.java

--- src/main/java/pl/touk/ticketBooking/domain/dataValidation/ValidateInput.java
+++ src/main/java/pl/touk/ticketBooking/domain/dataValidation/ValidateInput.java
@@ -1,7 +1,6 @@
 package pl.touk.ticketBooking.domain.dataValidation;
 
 import pl.touk.ticketBooking.domain.guest.Guest;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.regex.Matcher;
@@ -9,20 +8,21 @@ import java.util.regex.Pattern;
 
 public class ValidateInput {
 
+    private static final Pattern namePattern = Pattern.compile("[A-ZŻŹĆĄŚĘŁÓŃ]{1}[a-zżźćńółęąś]{2,20}");
+    private static final Pattern surnamePattern = Pattern.compile("[A-ZŻŹĆĄŚĘŁÓŃ]{1}[a-zżźćńółęąś]{2,20}+([-][A-ZŻŹĆĄŚĘŁÓŃ]{1}[a-zżźćńółęąś]{2,20})*");
+
     public static boolean validateNameAndSurname(Guest guest){
         return validateName(guest.getName()) && validateSurname(guest.getSurname());
     }
 
     private static boolean validateName(String name){
-        Pattern p = Pattern.compile("[A-ZŻŹĆĄŚĘŁÓŃ]{1}[a-zżźćńółęąś]{2,20}");
-        Matcher m = p.matcher(name);
-        return m.matches();
+        Matcher nameMatcher = namePattern.matcher(name);
+        return nameMatcher.matches();
     }
 
     private static boolean validateSurname(String surname){
-        Pattern p = Pattern.compile("[A-ZŻŹĆĄŚĘŁÓŃ]{1}[a-zżźćńółęąś]{2,20}+([-][A-ZŻŹĆĄŚĘŁÓŃ]{1}[a-zżźćńółęąś]{2,20})*");
-        Matcher m = p.matcher(surname);
-        return m.matches();
+        Matcher surnameMatcher = surnamePattern.matcher(surname);
+        return surnameMatcher.matches();
     }
 
     public static boolean validateReservationTime(LocalTime sessionTime, LocalDate sessionDate){
